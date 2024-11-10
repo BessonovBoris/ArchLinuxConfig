61667ea52dc36d1577ec3e7dd42b69dda8936ae4
Boris <borisimo.work@gmail.com>
2024-11-11 02:55:13 +0300

    edit: waybar\fastfetch modules, background script

--- bin/bin/change-wallpaper.sh
+++ bin/bin/change-wallpaper.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-name="$1"/$(ls "$1" | shuf -n1)
-hyprctl hyprpaper preload "$name"
-hyprctl hyprpaper wallpaper "HDMI-A-1,$name"
-hyprctl hyprpaper wallpaper "eDP-1,$name"
+wallpaper="$1"/$(ls "$1" | shuf -n1)
+
+hyprctl hyprpaper preload "$wallpaper"
+hyprctl hyprpaper wallpaper "HDMI-A-1,$wallpaper"
+hyprctl hyprpaper wallpaper "eDP-1,$wallpaper"
